65bc19d3f405047f557a8737d63f6a0c0b8e1e0a
zengjianping <happysjfzjp@126.com>
2024-02-04 17:31:14 +0800

    de

--- lib/eco/features/sse.hpp
+++ lib/eco/features/sse.hpp
@@ -5,7 +5,12 @@
 *******************************************************************************/
 #ifndef _SSE_HPP_
 #define _SSE_HPP_
-#include <emmintrin.h> // SSE2:<e*.h>, SSE3:<p*.h>, SSE4:<s*.h>
+
+#ifdef USE_NEON
+#include "sse2neon.h"
+#else
+#include <emmintrin.h>// SSE2:<e*.h>, SSE3:<p*.h>, SSE4:<s*.h>
+#endif
 
 #define RETf inline __m128
 #define RETi inline __m128i
